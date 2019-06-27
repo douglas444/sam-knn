39dc0b46fe38dfccb3a893c352d9176620ea39b5
Douglas <douglas.m.cavalcanti@gmail.com>
2019-06-27 04:59:51 -0300

    add examples

---
+++ src/main/java/br/com/douglas444/samknn/examples/InterchangingRBF.java
@@ -0,0 +1,47 @@
+package br.com.douglas444.samknn.examples;
+
+import br.com.douglas444.dsframework.DSException;
+import br.com.douglas444.dsframework.DSFileReader;
+import br.com.douglas444.dsframework.Point;
+import br.com.douglas444.samknn.SAMKNNController;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
+public class InterchangingRBF {
+    public static void main(String[] args) {
+
+        File dataFile = new File("./interchangingRBF.data");
+        File labelFile = new File("./interchangingRBF.labels");
+
+        DSFileReader dsFileReader;
+        try {
+            dsFileReader = new DSFileReader(" ", new FileReader(dataFile), new FileReader(labelFile));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            return;
+        }
+
+        SAMKNNController samknnController= new SAMKNNController();
+
+        int t = 0;
+        Point point;
+        while(true) {
+
+
+            try {
+                if ((point = dsFileReader.next()) == null) break;
+            } catch (DSException e) {
+                e.printStackTrace();
+                return;
+            }
+
+            samknnController.predictAndUpdate(point);
+
+            System.out.println(" Timestamp: " + (t++) + " Accuracy: " + samknnController.getAccuracy());
+        }
+
+    }
+
+}
